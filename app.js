d2a872c2e722f191cce67781c0873a1c38b7fbe7
Izzaldin Allawzi <izzaldin.allawzi@gmail.com>
2023-06-17 19:29:10 -0700

    creating the app and adding styles

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
-async function getGif(){
-    const res = await axios.get('http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym');
-    console.log(res);
+const input = document.querySelector("input");
+const createGif = document.querySelector("#submit")
+const form = document.querySelector("form")
+const gifCont = document.querySelector(".container")
+const deleteAll = document.querySelector("#delete")
+
+function makeGif(imgSrc){
+    
+ const div = document.createElement("div");
+ const img = document.createElement('img')
+ img.setAttribute("class","image")
+ div.setAttribute("class","gif")
+ img.src = imgSrc;
+ div.append(img);
+ gifCont.append(div);
+    
+
 }
+form.addEventListener("submit", async function(e){
+    e.preventDefault();
+    let inputVal = input.value;
+    input.value = '';
+    const res = await axios.get('http://api.giphy.com/v1/gifs/search',{params:{q:inputVal, api_key:"yGHOEJqyvnv15QJjNqlU0Ic0fafc9Qfe"}});
+    let randomIdx = Math.floor((Math.random() * 50)); 
+    console.log(res.data.data[randomIdx].url);
+    let imgSrc = res.data.data[randomIdx].images.original.url;
+    makeGif(imgSrc);
+})
+
+deleteAll.addEventListener('click',function(){
+    gifCont.innerHTML = '';
+})
